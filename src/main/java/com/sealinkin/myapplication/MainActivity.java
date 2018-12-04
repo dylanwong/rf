4f49bb12890499585a8f220ef40ef270517f77dc
915985615@qq.com <xtu915985615>
2018-12-04 11:57:24 +0800

    wyy

--- src/main/java/com/sealinkin/myapplication/MainActivity.java
+++ src/main/java/com/sealinkin/myapplication/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        setShowWhenLocked(true);
         setContentView(R.layout.activity_main);
     }
 }
